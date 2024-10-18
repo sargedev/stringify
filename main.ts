ddac0968cc09483e87cdfb01c1a1dcde10c0cee4
Sarge <63020240+sargedev@users.noreply.github.com>
2024-10-18 19:12:46 +0200

    Restore '5/4/2024, 9:08:01 PM Exported method and added docstring'

--- main.ts
+++ main.ts
@@ -17,12 +17,6 @@ namespace text {
     }
 
     function stringifyObject(value: object): string {
-        try {
-            return (value as any).toString();
-        } catch (e) {
-            // continue execution
-        }
-
         if (Array.isArray(value)) return stringifyArray(value as any[]);
         return stringifyRealObject(value as {[key: string]: any});
     }
